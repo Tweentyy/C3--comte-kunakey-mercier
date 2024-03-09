a61d465bfae489bd99c7c869ba7e92c95bfa4f25
Tweenty_ <tweenty.pro@gmail.com>
2024-03-09 11:31:39 +0100

    add: Générer 3 équipes de 2 personnes test for "TeamGenerator"

--- test/teamGenerator.test.js
+++ test/teamGenerator.test.js
@@ -12,3 +12,13 @@ describe("Tests sur le Team Generator", () => {
         expect(teams.getTeams()[1].players.length).to.equal(3);
         expect(teams.getTeams()[0].players).to.not.equal(teams.getTeams()[1].players);
     });
+
+    it("Générer 3 équipes de 2 personnes", () => {
+        const teams = new TeamGenerator(["Léo", "Bob", "Pierre", "Edouard", "Eva", "Frank"], 2);
+        teams.generateTeams();
+
+        expect(teams.getTeams().length).to.equal(3);
+        expect(teams.getTeams()[0].players.length).to.equal(2);
+        expect(teams.getTeams()[1].players.length).to.equal(2);
+        expect(teams.getTeams()[0].players).to.not.equal(teams.getTeams()[1].players).to.not.equal(teams.getTeams()[2].players);
+    });
