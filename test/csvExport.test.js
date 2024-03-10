42d76b6f78023b2e7b610b5a122627f6a90d2d7f
Tweenty_ <tweenty.pro@gmail.com>
2024-03-10 13:11:37 +0100

    add: Exporter un fichier CSV sans tableau test for csvExport

---
+++ test/csvExport.test.js
@@ -0,0 +1,10 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import CsvExport from "../src/csvExport.js";
+
+describe("Tests sur le CSV Export", () => {
+    it("Exporter un fichier CSV sans tableau", () => {
+        const data = "test";
+        expect(() => new CsvExport(data)).to.throw("Les données doivent être un tableau");
+    });
+});
