e0a9785ebe06b2b8ae2213d329f067db12a3c625
Tweenty_ <tweenty.pro@gmail.com>
2024-03-10 13:22:52 +0100

    add: all validators

--- src/csvExport.js
+++ src/csvExport.js
@@ -2,6 +2,11 @@ import { createObjectCsvWriter } from "csv-writer";
 
 class CsvExport {
     constructor(data, path = "../teams.csv") { // data représente une liste de { name: string, captain: string, players: string[] }
+        if (data.length === 0) throw new Error("Aucune donnée à exporter");
+        if (typeof path !== "string") throw new Error("Le chemin du fichier doit être une chaîne de caractères");
+        if (!Array.isArray(data)) throw new Error("Les données doivent être un tableau");
+        if (!data.every(team => team.name && team.captain && team.players)) throw new Error("Les données doivent être des objets avec les clés : name, captain et players");
+
         this.data = data;
         this.csvWriter = createObjectCsvWriter({
             path: path,
