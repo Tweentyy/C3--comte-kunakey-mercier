bb4a0448e1834af00bc13f3b4b049ecd7d30312a
Tweenty_ <tweenty.pro@gmail.com>
2024-03-10 13:17:25 +0100

    add: Exporter un fichier CSV avec des données valides test for csvExport

--- test/csvExport.test.js
+++ test/csvExport.test.js
@@ -1,6 +1,7 @@
 import { describe, it } from "mocha";
 import { expect } from "chai";
 import CsvExport from "../src/csvExport.js";
+import fs from "fs";
 
 describe("Tests sur le CSV Export", () => {
     it("Exporter un fichier CSV sans données", () => {
@@ -16,5 +17,18 @@ describe("Tests sur le CSV Export", () => {
     it("Exporter un fichier CSV avec des données invalides", () => {
         const data = [{ name: "Equipe 1", captain: "Leo", players: ["Léo", "Edouard", "Arthur"] }, { name: "Equipe 2", captain: "Edouard" }];
         expect(() => new CsvExport(data)).to.throw("Les données doivent être des objets avec les clés : name, captain et players");
+    });
+
+    it("Exporter un fichier CSV avec des données valides", async () => {
+        const data = [
+            { name: "Equipe 1", captain: "Leo", players: ["Léo", "Edouard", "Arthur"] },
+            { name: "Equipe 2", captain: "Edouard", players: ["Edouard", "Eva", "Frank"] }
+        ];
+
+        const path = "./test.csv"
+        const csvExport = new CsvExport(data, path);
+        await csvExport.export();
+
+        expect(fs.existsSync(path)).to.be.true;
     });
 });
