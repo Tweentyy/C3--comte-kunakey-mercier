d9705084a17674d4eb5589c712135bb44524988f
Tweenty_ <tweenty.pro@gmail.com>
2024-03-10 13:12:07 +0100

    add: Exporter un fichier CSV sans données test for csvExport

--- test/csvExport.test.js
+++ test/csvExport.test.js
@@ -3,6 +3,11 @@ import { expect } from "chai";
 import CsvExport from "../src/csvExport.js";
 
 describe("Tests sur le CSV Export", () => {
+    it("Exporter un fichier CSV sans données", () => {
+        const data = [];
+        expect(() => new CsvExport(data)).to.throw("Aucune donnée à exporter");
+    });
+
     it("Exporter un fichier CSV sans tableau", () => {
         const data = "test";
         expect(() => new CsvExport(data)).to.throw("Les données doivent être un tableau");
